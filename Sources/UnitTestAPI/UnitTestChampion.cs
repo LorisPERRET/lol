6bfa640fe90b299dfce71bfbd4d2cf450b5ed9e5
Loris PERRET <Loris.PERRET@etu.uca.fr>
2023-02-02 15:22:59 +0100

    :white_check_mark: Test unitaire GetChampions

--- Sources/UnitTestAPI/UnitTestChampion.cs
+++ Sources/UnitTestAPI/UnitTestChampion.cs
@@ -1,11 +1,61 @@
+using API.Controllers;
+using DTO_API;
+using Microsoft.AspNetCore.Mvc;
+using StubLib;
+
 namespace UnitTestAPI
 {
     [TestClass]
     public class UnitTestChampion
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task TestGetChampions()
         {
+            StubData stub = new StubData();
+            ChampionsController controller = new ChampionsController(stub);
+            
+            var result = await controller.Get() as OkObjectResult;
+            
+            Assert.IsNotNull(result);
+            Assert.AreEqual(200, result.StatusCode);
+
+            var champions = result.Value as IEnumerable<ChampionDto>;
+
+            Assert.IsNotNull(champions);
+            Assert.AreEqual(await stub.ChampionsMgr.GetNbItems(),champions.Count());
+
+        }
+
+        [TestMethod]
+        public async Task TestGetChampion()
+        {
+            StubData stub = new StubData();
+            ChampionsController controller = new ChampionsController(stub);
+            
+        }
+
+        [TestMethod]
+        public async Task TestPostChampion()
+        {
+            StubData stub = new StubData();
+            ChampionsController controller = new ChampionsController(stub);
+            
+        }
+
+        [TestMethod]
+        public async Task TestPutChampion()
+        {
+            StubData stub = new StubData();
+            ChampionsController controller = new ChampionsController(stub);
+            
+        }
+
+        [TestMethod]
+        public async Task TestDeleteChampion()
+        {
+            StubData stub = new StubData();
+            ChampionsController controller = new ChampionsController(stub);
+            
         }
     }
 }
