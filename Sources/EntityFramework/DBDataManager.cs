125cb7e637096dcb01735a2a48da822271ba518b
Loris PERRET <Loris.PERRET@etu.uca.fr>
2023-02-04 08:15:31 +0100

    :sparkles: Client EF operation CRUD

---
+++ Sources/EntityFramework/DBDataManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO_EF;
+using Model;
+using Shared;
+
+namespace EntityFramework
+{
+    public class DBDataManager : IGenericDataManager<Champion>
+    {
+        public Task<Champion> AddItem(Champion item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteItem(Champion item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Champion>> GetItems(int index, int count, string? orderingPropertyName = null, bool descending = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Champion>> GetItemsByName(string substring, int index, int count, string? orderingPropertyName = null, bool descending = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetNbItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetNbItemsByName(string substring)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Champion> UpdateItem(Champion oldItem, Champion newItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Sources/TestEF/OpérationCRUD.cs
+++ Sources/TestEF/OpérationCRUD.cs
@@ -19,12 +19,6 @@ Console.WriteLine("Hello, World!");
 using (var context = new SqlLiteDbContext())
 {
     Console.WriteLine("Test Opération CRUD : Read");
-    /*foreach (var champion in context.Champions)
-    {
-        Console.WriteLine($"{champion}");
-    }*/
-
-    //context.Champions.ToList().ForEach(c => Console.WriteLine(c.Name));
 
     context.Database.EnsureCreated();
     foreach (var champion in context.Champions)
