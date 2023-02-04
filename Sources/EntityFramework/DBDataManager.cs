8fe0435d397a0c8c8a28d8965366eef0f315990d
Hugo LIVET <Hugo.LIVET@etu.uca.fr>
2023-02-04 09:38:13 +0100

    :sparkles: ClientEF Gets

--- Sources/EntityFramework/DBDataManager.cs
+++ Sources/EntityFramework/DBDataManager.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DTO_EF;
+using DTO_EF.Mapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Model;
 using Shared;
 
@@ -23,12 +27,22 @@ namespace EntityFramework
 
         public Task<IEnumerable<Champion>> GetItems(int index, int count, string? orderingPropertyName = null, bool descending = false)
         {
-            throw new NotImplementedException();
+            using (var context = new SqlLiteDbContext())
+            {
+                var temp = context.Champions.Skip(index * count).Take(count);
+                temp = GetItemWithFilter(orderingPropertyName, descending, temp);
+                return Task.FromResult(temp.ToList().ToChampions());
+            }
         }
 
         public Task<IEnumerable<Champion>> GetItemsByName(string substring, int index, int count, string? orderingPropertyName = null, bool descending = false)
         {
-            throw new NotImplementedException();
+            using(var context = new SqlLiteDbContext())
+            {
+                var temp = context.Champions.Where(c => c.Name == substring).Skip(index * count).Take(count);
+                temp = GetItemWithFilter(orderingPropertyName, descending, temp);
+                return Task.FromResult(temp.ToList().ToChampions());
+            }
         }
 
         public Task<int> GetNbItems()
@@ -45,5 +59,21 @@ namespace EntityFramework
         {
             throw new NotImplementedException();
         }
+
+        private static IQueryable<ChampionEntity> GetItemWithFilter(string? orderingPropertyName, bool descending, IQueryable<ChampionEntity> temp)
+        {
+            if (orderingPropertyName != null)
+            {
+                var prop = typeof(ChampionEntity).GetProperty(orderingPropertyName!);
+                if (prop != null)
+                {
+                    temp = descending ? temp.OrderByDescending(item => prop.GetValue(item))
+                                        : temp.OrderBy(item => prop.GetValue(item));
+                }
+            }
+
+            return temp;
+        }
+
     }
 }
