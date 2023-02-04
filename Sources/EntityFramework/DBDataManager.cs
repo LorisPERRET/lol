8265d4c71bc08ace10a4453cda371da7f2c2d33c
Loris PERRET <Loris.PERRET@etu.uca.fr>
2023-02-04 08:32:00 +0100

    :sparkles: Add et Delete client EF

--- Sources/EntityFramework/DBDataManager.cs
+++ Sources/EntityFramework/DBDataManager.cs
@@ -4,21 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DTO_EF;
+using DTO_EF.Mapper;
 using Model;
 using Shared;
 
 namespace EntityFramework
 {
-    public class DBDataManager : IGenericDataManager<Champion>
+    public class DbDataManager : IGenericDataManager<Champion>
     {
         public Task<Champion> AddItem(Champion item)
         {
-            throw new NotImplementedException();
+            using (var context = new SqlLiteDbContext())
+            {
+                context.Champions.Add(item.ToEntity());
+                return Task.FromResult<Champion>(item);
+            }
         }
 
         public Task<bool> DeleteItem(Champion item)
         {
-            throw new NotImplementedException();
+            using (var context = new SqlLiteDbContext())
+            {
+                var result = context.Champions.Remove(item.ToEntity());
+                if (result == null)
+                {
+                    return Task.FromResult<bool>(false);
+                }
+
+                return Task.FromResult<bool>(true);
+            }
         }
 
         public Task<IEnumerable<Champion>> GetItems(int index, int count, string? orderingPropertyName = null, bool descending = false)
