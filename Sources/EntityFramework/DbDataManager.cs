b78e64f3d3ace67d003e5bd0e734eecefe391186
Loris PERRET <Loris.PERRET@etu.uca.fr>
2023-02-08 14:51:52 +0100

    :sparkles: ctor manager db

--- Sources/EntityFramework/DbDataManager.cs
+++ Sources/EntityFramework/DbDataManager.cs
@@ -7,85 +7,71 @@ namespace EntityFramework
 {
     public class DbDataManager : IGenericDataManager<Champion>
     {
+        private SqlLiteDbContext _context;
+
+        public DbDataManager(SqlLiteDbContext context)
+        {
+            _context = context;
+        }
+
         public Task<Champion> AddItem(Champion item)
         {
-            using (var context = new SqlLiteDbContext())
-            {
-                context.Database.EnsureCreated();
-                context.Champions.Add(item.ToEntity());
-                context.SaveChanges();
-                return Task.FromResult<Champion>(item);
-            }
+            _context.Database.EnsureCreated();
+            _context.Champions.Add(item.ToEntity());
+            _context.SaveChanges();
+            return Task.FromResult<Champion>(item);
         }
 
         public Task<bool> DeleteItem(Champion item)
         {
-            using (var context = new SqlLiteDbContext())
-            {
-                context.Database.EnsureCreated();
-                var result = context.Champions.Remove(item.ToEntity());
-                context.SaveChanges();
-                return Task.FromResult<bool>(result != null);
-            }
+            _context.Database.EnsureCreated();
+            var result = _context.Champions.Remove(item.ToEntity());
+            _context.SaveChanges();
+            return Task.FromResult<bool>(result != null);
         }
 
         public Task<IEnumerable<Champion>> GetItems(int index, int count, string? orderingPropertyName = null, bool descending = false)
         {
-            using (var context = new SqlLiteDbContext())
-            {
-                var temp = context.Champions.Skip(index * count).Take(count);
-                temp = GetItemWithFilter(orderingPropertyName, descending, temp);
-                return Task.FromResult(temp.ToList().ToChampions());
-            }
+            var temp = _context.Champions.Skip(index * count).Take(count);
+            temp = GetItemWithFilter(orderingPropertyName, descending, temp);
+            return Task.FromResult(temp.ToList().ToChampions());
         }
 
         public Task<IEnumerable<Champion>> GetItemsByName(string substring, int index, int count, string? orderingPropertyName = null, bool descending = false)
         {
-            using (var context = new SqlLiteDbContext())
-            {
-                var temp = context.Champions.Where(c => c.Name == substring).Skip(index * count).Take(count);
-                temp = GetItemWithFilter(orderingPropertyName, descending, temp);
-                return Task.FromResult(temp.ToList().ToChampions());
-            }
+            var temp = _context.Champions.Where(c => c.Name == substring).Skip(index * count).Take(count);
+            temp = GetItemWithFilter(orderingPropertyName, descending, temp);
+            return Task.FromResult(temp.ToList().ToChampions());
         }
 
         public Task<int> GetNbItems()
         {
-            using (var context = new SqlLiteDbContext())
-            {
-                context.Database.EnsureCreated();
-                var count = context.Champions.Count();
-                return Task.FromResult<int>(count);
-            }
+            _context.Database.EnsureCreated();
+            var count = _context.Champions.Count();
+            return Task.FromResult<int>(count);
         }
 
         public Task<int> GetNbItemsByName(string substring)
         {
-            using (var context = new SqlLiteDbContext())
-            {
-                context.Database.EnsureCreated();
-                var count = context.Champions.Count(item => item.Name == substring);
-                return Task.FromResult<int>(count);
-            }
+            _context.Database.EnsureCreated();
+            var count = _context.Champions.Count(item => item.Name == substring);
+            return Task.FromResult<int>(count);
         }
 
         public Task<Champion> UpdateItem(Champion oldItem, Champion newItem)
         {
-            using (var context = new SqlLiteDbContext())
+            _context.Database.EnsureCreated();
+            if (oldItem == null || newItem == null) return Task.FromResult<Champion>(default(Champion));
+
+            if (!_context.Champions.Contains(oldItem.ToEntity()))
             {
-                context.Database.EnsureCreated();
-                if (oldItem == null || newItem == null) return Task.FromResult<Champion>(default(Champion));
-
-                if (!context.Champions.Contains(oldItem.ToEntity()))
-                {
-                    return Task.FromResult<Champion>(default(Champion));
-                }
-
-                context.Champions.Remove(oldItem.ToEntity());
-                context.Champions.Add(newItem.ToEntity());
-                context.SaveChanges();
-                return Task.FromResult<Champion>(newItem);
+                return Task.FromResult<Champion>(default(Champion));
             }
+
+            _context.Champions.Remove(oldItem.ToEntity());
+            _context.Champions.Add(newItem.ToEntity());
+            _context.SaveChanges();
+            return Task.FromResult<Champion>(newItem);
         }
 
         private static IQueryable<ChampionEntity> GetItemWithFilter(string? orderingPropertyName, bool descending, IQueryable<ChampionEntity> temp)
