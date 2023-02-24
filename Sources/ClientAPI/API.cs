5e2def41beb5caed3ee54e9c9fe332f08fb39568
Hugo LIVET <Hugo.LIVET@etu.uca.fr>
2023-02-24 14:51:53 +0100

    Ajout de l'application MAUI et ajout des classes de clientAPI (non implémenté)

---
+++ Sources/ClientAPI/API.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientAPI
+{
+    public class API : IDataManager
+    {
+        public IChampionsManager ChampionsMgr => throw new NotImplementedException();
+
+        public ISkinsManager SkinsMgr => throw new NotImplementedException();
+
+        public IRunesManager RunesMgr => throw new NotImplementedException();
+
+        public IRunePagesManager RunePagesMgr => throw new NotImplementedException();
+    }
+}
---
+++ Sources/LolApp/ViewModels/ApplicationVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CommunityToolkit.Mvvm.Input;
+using Model;
+using ViewModels;
+
+namespace LolApp.ViewModels
+{
+	public class ApplicationVM
+	{
+		internal IDataManager DataMgr { get; set; }
+
+		public ChampionsMgrVM ChampionsMgrVM { get; set; } 
+
+		public ApplicationVM(IDataManager dataManager)
+		{
+			DataMgr = dataManager;
+			ChampionsMgrVM = new ChampionsMgrVM(DataMgr);
+		}
+
+		public IRelayCommand<ChampionVM> NavigateToChampionDetailsPageCommand { get; }
+			= new RelayCommand<ChampionVM>(async cvm => await App.Current.MainPage.Navigation.PushAsync(new ChampionPage(cvm)));
+	}
+}
+
