38d7a0c36a26c8cea9c8a4f8d06c64ba24641443
Loris PERRET <Loris.PERRET@etu.uca.fr>
2023-03-03 15:53:06 +0100

    :sparkles: One to Many

--- Sources/EntityFramework/SqlLiteDbContextWithStub.cs
+++ Sources/EntityFramework/SqlLiteDbContextWithStub.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Rune = Model.Rune;
 
 namespace EntityFramework
 {
@@ -16,12 +17,30 @@ namespace EntityFramework
         public StubData _dataManager = new StubData();
         protected async override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            IEnumerable<Champion> champ = await _dataManager.ChampionsMgr.GetItems(0,
-                await _dataManager.ChampionsMgr.GetNbItems());
-
             base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<ChampionEntity>().HasData(
-                champ.ToEntities()
+                new ChampionEntity { Name = "Akali", Bio = "", Icon = "", Class = ChampionClass.Assassin.ToString(), Image = "" },
+                new ChampionEntity { Name = "Aatrox", Bio = "", Icon = "", Class = ChampionClass.Fighter.ToString(), Image = "" }
+            );
+            modelBuilder.Entity<SkinEntity>().HasData(
+                new SkinEntity("Stinger", "Akali"),
+                new SkinEntity("Infernal", "Akali"),
+                new SkinEntity("All-Star", "Akali"),
+                new SkinEntity("Justicar", "Aatrox"),
+                new SkinEntity("Mecha", "Aatrox"),
+                new SkinEntity("Sea Hunter", "Aatrox"),
+                new SkinEntity("Dynasty", "Ahri"),
+                new SkinEntity("Midnight", "Ahri"),
+                new SkinEntity("Foxfire", "Ahri"),
+            );
+            modelBuilder.Entity<RuneEntity>().HasData(
+                new RuneEntity("Conqueror", RuneFamily.Precision.ToString()),
+                new RuneEntity("Triumph", RuneFamily.Precision.ToString()),
+                new RuneEntity("Legend: Alacrity", RuneFamily.Precision.ToString()),
+                new RuneEntity("Legend: Tenacity", RuneFamily.Precision.ToString()),
+                new RuneEntity("last stand", RuneFamily.Domination.ToString()),
+                new RuneEntity("last stand 2", RuneFamily.Domination.ToString())
             );
         }
     }
