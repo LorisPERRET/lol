ee0095e19e3f6f7a59beab8b9b7bea0b9d01ea8b
Loris PERRET <Loris.PERRET@etu.uca.fr>
2023-03-06 17:48:12 +0100

    :sparkles: Ajouter de try catch

--- Sources/API/Controllers/SkinController.cs
+++ Sources/API/Controllers/SkinController.cs
@@ -3,6 +3,7 @@ using DTO_API.Mapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Model;
+using System.Net;
 
 namespace API.Controllers
 {
@@ -11,72 +12,125 @@ namespace API.Controllers
     public class SkinController : ControllerBase
     {
         private readonly IDataManager _dataManager;
-        public SkinController(IDataManager dataManager)
+        private readonly ILogger _logger;
+
+        public SkinController(IDataManager dataManager, ILogger logger)
         {
             _dataManager = dataManager;
+            _logger = logger;
         }
 
         // GET: api/<SkinController>
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            IEnumerable<Skin> skins = await _dataManager.SkinsMgr.GetItems(0, await _dataManager.SkinsMgr.GetNbItems());
-            if (skins.Count() == 0)
+            try
             {
-                return NoContent();
+                IEnumerable<Skin> skins = await _dataManager.SkinsMgr.GetItems(0, await _dataManager.SkinsMgr.GetNbItems());
+                if (skins.Count() == 0)
+                {
+                    _logger.LogInformation("Aucun skin n'a été trouvé");
+                    return NoContent();
+                }
+                return Ok(skins.ToDtos());
+
+            } catch (Exception)
+            {
+                _logger.LogWarning("Une erreur est survenue en lien avec le serveur");
+                return StatusCode(((int)HttpStatusCode.InternalServerError), new { message = "Erreur interne du serveur." });
             }
-            return Ok(skins.ToDtos());
         }
 
         // GET api/<SkinController>/5
         [HttpGet("{nom}")]
         public async Task<IActionResult> Get(string nom)
         {
-            IEnumerable<Skin> skin = await _dataManager.SkinsMgr.GetItemsByName(nom, 0,
-                await _dataManager.SkinsMgr.GetNbItemsByName(nom), null);
-            if (skin.Count() == 0)
+            try
             {
-                return NoContent();
+                IEnumerable<Skin> skin = await _dataManager.SkinsMgr.GetItemsByName(nom, 0,
+                    await _dataManager.SkinsMgr.GetNbItemsByName(nom), null);
+                if (skin.Count() == 0)
+                {
+                    _logger.LogInformation("Aucun skin n'a été trouvé");
+                    return NoContent();
+                }
+                return Ok(skin.Single().ToDto());
+
             }
-            return Ok(skin.Single().ToDto());
+            catch (Exception)
+            {
+                _logger.LogWarning("Une erreur est survenue en lien avec le serveur");
+                return StatusCode(((int)HttpStatusCode.InternalServerError), new { message = "Erreur interne du serveur." });
+            }
         }
 
         // POST api/<SkinController>
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] SkinDto skin)
         {
-            if (await _dataManager.SkinsMgr.GetNbItemsByName(skin.Name) == 0)
+            try
             {
-                await _dataManager.SkinsMgr.AddItem(skin.ToSkin());
-                return CreatedAtAction(nameof(Get), skin.Name, skin);
+                if (await _dataManager.SkinsMgr.GetNbItemsByName(skin.Name) == 0)
+                {
+                    await _dataManager.SkinsMgr.AddItem(skin.ToSkin());
+                    return CreatedAtAction(nameof(Get), skin.Name, skin);
+                }
+                _logger.LogInformation("Le skin voulu est déja existant");
+                return BadRequest();
             }
-            return BadRequest();
+            catch (Exception)
+            {
+                _logger.LogWarning("Une erreur est survenue en lien avec le serveur");
+                return StatusCode(((int)HttpStatusCode.InternalServerError), new { message = "Erreur interne du serveur." });
+            }
         }
 
         // PUT api/<SkinController>/5
         [HttpPut("{nom}")]
         public async Task<IActionResult> Put(string nom, [FromBody] SkinDto skin)
         {
-            if (!nom.Equals(skin.Name)) return BadRequest();
-            var oldSkin = await _dataManager.SkinsMgr.GetItemsByName(nom, 0,
-                await _dataManager.SkinsMgr.GetNbItemsByName(nom));
+            try
+            {
+                if (!nom.Equals(skin.Name))
+                {
+                    _logger.LogInformation("Vous ne pouvez pas modifier le nom d'un skin");
+                    return BadRequest();
+                }
+                var oldSkin = await _dataManager.SkinsMgr.GetItemsByName(nom, 0,
+                    await _dataManager.SkinsMgr.GetNbItemsByName(nom));
 
-            var newSkin = await _dataManager.SkinsMgr.UpdateItem(oldSkin.FirstOrDefault(), skin.ToSkin());
-            return Ok(newSkin.ToDto());
+                var newSkin = await _dataManager.SkinsMgr.UpdateItem(oldSkin.FirstOrDefault(), skin.ToSkin());
+                return Ok(newSkin.ToDto());
+
+            }
+            catch (Exception)
+            {
+                _logger.LogWarning("Une erreur est survenue en lien avec le serveur");
+                return StatusCode(((int)HttpStatusCode.InternalServerError), new { message = "Erreur interne du serveur." });
+            }
         }
 
         // DELETE api/<SkinController>/5
         [HttpDelete("{nom}")]
         public async Task<IActionResult> Delete(string nom)
         {
-            var skin = await _dataManager.SkinsMgr.GetItemsByName(nom, 0, await _dataManager.SkinsMgr.GetNbItemsByName(nom));
-
-            bool res = await _dataManager.SkinsMgr.DeleteItem(skin.FirstOrDefault());
-            if (res)
+            try
             {
-                return Ok(nom);
+                var skin = await _dataManager.SkinsMgr.GetItemsByName(nom, 0, await _dataManager.SkinsMgr.GetNbItemsByName(nom));
+
+                bool res = await _dataManager.SkinsMgr.DeleteItem(skin.FirstOrDefault());
+                if (res)
+                {
+                    return Ok(nom);
+                }
+                _logger.LogInformation("Le skin n'a pas été supprimer");
+                return BadRequest();
             }
-            return BadRequest();
+            catch (Exception)
+            {
+                _logger.LogWarning("Une erreur est survenue en lien avec le serveur");
+                return StatusCode(((int)HttpStatusCode.InternalServerError), new { message = "Erreur interne du serveur." });
+            }
         }
     }
 }
